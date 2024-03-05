257b0318b3db63cbc09da35a9030554c4329cdab
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-03-05 17:02:07 -0300

    Update v1.2.5
    - Fundamentos: Template Expressions

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.js
@@ -0,0 +1,19 @@
+const TemplateExpressions = () => {
+
+    const name = "Douglas";
+    const data = {
+        age: 31,
+        job: "Developer",
+    }
+
+    return (
+        <div>
+            <h1>Olá { name }, tudo bem?</h1>
+            <p>Você atua como: { data.job }</p>
+            <p>{ 4 + 4 }</p>
+            <p>{ console.log("JSX React") }</p>
+        </div>
+    )
+}
+
+export default TemplateExpressions;
