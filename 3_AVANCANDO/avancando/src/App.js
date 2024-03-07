0c34b91fdf50f3a9f717bbfe8b3df8aee0f2075f
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-03-07 19:32:29 -0300

    Update v1.3.3
    - Avançando: useState Hook

--- 3_AVANCANDO/avancando/src/App.js
+++ 3_AVANCANDO/avancando/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import Plunderer from './assets/img2.webp'
+import ManageData from './components/ManageData';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <div>
         <img src={Plunderer} alt="Plunderer Licht" />
       </div>
+      <ManageData />
     </div>
   );
 }
