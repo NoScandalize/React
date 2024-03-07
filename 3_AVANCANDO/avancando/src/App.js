14c6891a92b77466e337b8f105de4ca237935ba3
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-03-07 18:38:08 -0300

    Update v1.3.1
    - Avançando: Imagens em React

---
+++ 3_AVANCANDO/avancando/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.webp" alt="TOMAN" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
