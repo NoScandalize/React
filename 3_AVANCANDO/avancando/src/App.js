dfb3191b08c604eb7a542b7193297d2685accef4
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-03-09 18:15:57 -0300

    Update v1.3.10
    - Avançando: Desestruturando props

--- 3_AVANCANDO/avancando/src/App.js
+++ 3_AVANCANDO/avancando/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 
-import Plunderer from './assets/img2.webp'
-import ListRender from './components/ListRender';
-import ManageData from './components/ManageData';
-import ConditionalRender from './components/ConditionalRender';
-import ShowUserName from './components/ShowUserName';
-
+import Plunderer from "./assets/img2.webp";
+import ListRender from "./components/ListRender";
+import ManageData from "./components/ManageData";
+import ConditionalRender from "./components/ConditionalRender";
+import ShowUserName from "./components/ShowUserName";
+import CarDetails from "./components/CarDetails";
 
 function App() {
   return (
@@ -23,6 +23,7 @@ function App() {
       <ListRender />
       <ConditionalRender />
       <ShowUserName name="Douglas" />
+      <CarDetails brand="VW" km={100000} color="Azul" />
     </div>
   );
 }
