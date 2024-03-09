e4fb4aba3e58260cced33cd7324d5c4496f486e3
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-03-09 18:29:39 -0300

    Update v1.3.12
    - Avançando: Reutilização com loop

--- 3_AVANCANDO/avancando/src/App.js
+++ 3_AVANCANDO/avancando/src/App.js
@@ -8,6 +8,12 @@ import ShowUserName from "./components/ShowUserName";
 import CarDetails from "./components/CarDetails";
 
 function App() {
+  const cars = [
+    { id: 1, brand: "Ferrari", color: "Amarela", newCar: true, Km: 0 },
+    { id: 2, brand: "KIA", color: "Branco", newCar: false, Km: 45333 },
+    { id: 3, brand: "Renault", color: "Azul", newCar: false, Km: 54000 },
+  ];
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -26,6 +32,14 @@ function App() {
       <CarDetails brand="VW" km={100000} color="Azul" newCar={false} />
       <CarDetails band="Ford" color="Vermelha" km={0} newCar={true} />
       <CarDetails band="Fiat" color="Branco" km={4500} newCar={false} />
+      {cars.map((car) => (
+        <CarDetails
+          brand={car.brand}
+          color={car.color}
+          Km={car.Km}
+          newCar={car.newCar}
+        />
+      ))}
     </div>
   );
 }
