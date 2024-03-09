2d50ac0f218f2adafce1c2a8f8afdab7f933498e
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-03-09 17:23:02 -0300

    Update v1.3.8
    - Avançando: Adicionando um else

--- 3_AVANCANDO/avancando/src/components/ConditionalRender.js
+++ 3_AVANCANDO/avancando/src/components/ConditionalRender.js
@@ -1,16 +1,28 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const ConditionalRender = () => {
-
   const [x] = useState(false);
+
+  const [name, setName] = useState("");
 
   return (
     <div>
       <h1>Isso será exibido?</h1>
       {x && <p>Se x for true, sim!</p>}
       {!x && <p>Agora x é falso!</p>}
+      <h1>If ternário</h1>
+      {name === "Douglas" ? (
+        <div>
+          <p>O nome é {name}</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado!</p>
+        </div>
+      )}
+      <button onClick={() => setName("Douglas")}>Clique aqui!</button>
     </div>
-  )
-}
+  );
+};
 
 export default ConditionalRender;
