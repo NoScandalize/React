1af691305a724405e3bfb58cf31077922c3588eb
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-03-15 17:51:42 -0300

    Update v1.4.3
    CSS: Inline style

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -7,6 +7,12 @@ function App() {
       <h1>React com CSS</h1>
       <MyComponent />
       <p>Este é o parágrafo do componente</p>
+      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      <p style={{ color: "magenta", padding: "25px", borderTop: "2px solid red" }}>
+        Este elemento foi estilizado de forma inline
+      </p>
     </div>
   );
 }
