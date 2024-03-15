d61aa6e68eddd43cdc9d4ffe2185dc7d0cb4ae1b
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-03-15 17:41:48 -0300

    Update v1.4.2
    CSS: CSS de Componente

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
     <div className="App">
       <h1>React com CSS</h1>
+      <MyComponent />
+      <p>Este é o parágrafo do componente</p>
     </div>
   );
 }
