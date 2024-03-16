af881f8621243b1042093adaca1be38370444181
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-03-16 19:25:43 -0300

    Update v1.5.3
    - Form: Manipulação de valores

--- 5_FORM_REACT/form/src/components/MyForm.js
+++ 5_FORM_REACT/form/src/components/MyForm.js
@@ -1,16 +1,26 @@
 import './MyForm.css'
 
+import { useState } from 'react';
+
 const MyForm = () => {
+
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
   return (
     <div>
         <form>
             <div>
                 <label htmlFor="name">Nome: </label>
-                <input type="text" name='name' placeholder="Digite o seu nome" />
+                <input type="text" name='name' placeholder='Digite o seu nome' onChange={handleName} />
             </div>
             <label>
                 <span>E-mail</span>
-                <input type="email" name="email" placeholder="Digite o seu email" />
+                <input type="email" name="email" placeholder='Digite o seu email' />
             </label>
             <input type="submit" value="Enviar" />
         </form>
