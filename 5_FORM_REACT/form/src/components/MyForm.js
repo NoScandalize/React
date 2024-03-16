361b6e1f040c342086a39096ea24d0bdaee539b2
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-03-16 19:00:43 -0300

    Update v1.5.2
    - Form: Label envolvendo o input

--- 5_FORM_REACT/form/src/components/MyForm.js
+++ 5_FORM_REACT/form/src/components/MyForm.js
@@ -8,6 +8,10 @@ const MyForm = () => {
                 <label htmlFor="name">Nome: </label>
                 <input type="text" name='name' placeholder="Digite o seu nome" />
             </div>
+            <label>
+                <span>E-mail</span>
+                <input type="email" name="email" placeholder="Digite o seu email" />
+            </label>
             <input type="submit" value="Enviar" />
         </form>
     </div>
