7fd256496edc2927f93b341a89ef7b583b551910
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2024-03-16 19:35:49 -0300

    Update v1.5.5
    - Form: Envio de formulário

--- 5_FORM_REACT/form/src/components/MyForm.js
+++ 5_FORM_REACT/form/src/components/MyForm.js
@@ -1,31 +1,47 @@
-import './MyForm.css'
+import "./MyForm.css";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 const MyForm = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
 
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
 
-    const handleName = (e) => {
-        setName(e.target.value)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Enviando o formulário!");
+    console.log(name, email);
+  };
 
   return (
     <div>
-        <form>
-            <div>
-                <label htmlFor="name">Nome: </label>
-                <input type="text" name='name' placeholder='Digite o seu nome' onChange={handleName} />
-            </div>
-            <label>
-                <span>E-mail</span>
-                <input type="email" name="email" placeholder='Digite o seu email' onChange={(e) => setEmail(e.target.value)} />
-            </label>
-            <input type="submit" value="Enviar" />
-        </form>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Nome: </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={handleName}
+          />
+        </div>
+        <label>
+          <span>E-mail</span>
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default MyForm;
